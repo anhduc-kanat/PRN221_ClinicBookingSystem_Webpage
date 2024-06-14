3cea451b6e03743787cce4cc2a6b179a501c5d96
unknown <kalen153246789@gmail.com>
2024-06-14 09:06:43 +0200

    fetch api services, dentist, date, slot for booking process

---
+++ src/pages/PricePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PricePage() {
+    return (
+        <div >
+            <h1>Price Page</h1>
+        </div>
+    )
+}
---
+++ src/pages/ServicePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Services from "../components/Sections/Services";
+
+export default function ServicePage() {
+    return (
+        <>
+             <Services />
+        </>
+    )
+}
