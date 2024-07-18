dae8c459593535db178bc40f9c461c6afe07e874
unknown <kalen153246789@gmail.com>
2024-07-18 13:26:36 +0700

    Change Password

--- src/pages/Dentist/DentistProfile.jsx
+++ src/pages/Dentist/DentistProfile.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Input, DatePicker, Col, Row, Button } from 'antd';
+import { Form, Input, DatePicker, Col, Row, Button, Modal, message } from 'antd';
 import { Helmet } from 'react-helmet-async';
 import dayjs from 'dayjs';
 import './Dentists.css';
+import axios from 'axios';
+
+const apiRoot = import.meta.env.VITE_API_ROOT;
 
 export default function DentistProfile() {
   const [profile, setProfile] = useState(null);
   const [responsibleServices, setResponsibleServices] = useState([]);
+  const [isChangePasswordModal, setIsChangePasswordModal] = useState(false);
+  const [form] = Form.useForm();
 
   useEffect(() => {
     const storedProfile = JSON.parse(localStorage.getItem('profile'));
@@ -18,8 +23,40 @@ export default function DentistProfile() {
     return <div>Loading...</div>;
   }
 
-  const handleChangePassword = () => {
+  const handleOpenChangePasswordModal = () => {
+    setIsChangePasswordModal(true);
+  }
 
+  const handleChangePassword = async () => {
+    const values = await form.validateFields();
+    const { oldPassword, newPassword } = values;
+    await axios.post(`${apiRoot}/user/change-password/${profile.id}`, null,
+      {
+        params: {
+          oldPassword,
+          newPassword
+        }
+      }
+    ).then(res => {
+      console.log(res)
+      if (res.data.statusCode === 200 || res.data.statusCode === 201) {
+        message.success(res.data.message);
+        setIsChangePasswordModal(false);
+        form.resetFields();
+      } else {
+        message.error(res.data.message);
+      }
+    }).catch(err => {
+      console.log(err)
+      message.error("Wrong password");
+    })
+
+
+  }
+
+  const handleCancelChangePassword = () => {
+    setIsChangePasswordModal(false);
+    form.resetFields();
   }
 
   return (
@@ -74,7 +111,7 @@ export default function DentistProfile() {
         <div>
           <p className='profile-title'>Responsible Services</p>
           {responsibleServices.length > 0 ? (
-            responsibleServices.map((service, index ) => (
+            responsibleServices.map((service, index) => (
               <li key={index}>{service.name}</li>
             ))
           ) : (
@@ -84,7 +121,41 @@ export default function DentistProfile() {
           )}
         </div>
       </div>
-      <Button style={{width: '30%'}}  onclick={handleChangePassword} >Change Password</Button>
+      <Button style={{ width: '30%' }} onClick={handleOpenChangePasswordModal}>Change Password</Button>
+      <Modal title="Change Password" open={isChangePasswordModal} onOk={handleChangePassword} onCancel={handleCancelChangePassword}>
+        <Form
+          form={form}
+          variant='filled'
+          style={{
+            maxWidth: 600,
+          }}
+        >
+          <Form.Item
+            label="Old Password"
+            name='oldPassword'
+            rules={[
+              {
+                required: true,
+                message: "Please input your old password"
+              }
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="New Password"
+            name="newPassword"
+            rules={[
+              {
+                required: true,
+                message: "Please input your new password"
+              }
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   );
 }
